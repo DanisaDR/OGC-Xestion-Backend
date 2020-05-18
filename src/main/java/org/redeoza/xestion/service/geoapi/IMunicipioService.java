c4f6500f20d458c714aa77c3f73d1bd548fa6bc6
Daniel <isasi.daniel@gmail.com>
2020-05-19 00:19:15 +0200

    engadido IMunSrv que non quedou incluido

---
+++ src/main/java/org/redeoza/xestion/service/geoapi/IMunicipioService.java
@@ -0,0 +1,15 @@
+package org.redeoza.xestion.service.geoapi;
+
+import org.redeoza.xestion.model.geoapi.Municipio;
+import org.redeoza.xestion.utils.GeoAPIEntities;
+
+import java.util.Set;
+
+public interface IMunicipioService {
+
+    Set<Municipio> getAllMunicipios();
+
+    void saveMun(Municipio municipio);
+
+    void checkAllMunWithGeoAPI(Set<GeoAPIEntities> lstGeoAPI);
+}
